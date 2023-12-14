760b3ab0c76671623bc77dbdc6e0b95bb1bde64e
Elvis2347 <elvisgomes47@gmail.com>
2023-12-14 20:57:08 -0300

    Adicionando Detalhes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Trilha JS Developer - Pokedex
+
+Adicionando Modificações para realizar o desafio proposto.
--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type}" data-pokemon-id="${pokemon.number}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -23,11 +23,47 @@ function convertPokemonToLi(pokemon) {
     `
 }
 
+function loadPokemonDetails (pokemonId) {
+    pokeApi.getPokemon(pokemonId).then((pokemon) => {
+        const pokemonName = document.getElementById('pokemonName');
+        const pokemonType = document.getElementById('pokemonType');
+        const pokemonImage = document.getElementById('.pokemon-image img');
+
+        pokemonName.textContent = pokemon.name;
+        pokemonType.textContent = pokemon.type;
+        pokemonImage.scr = pokemon.photo;
+    });
+}
+
+function loadPokemonDetails(pokemonId) {
+    pokeApi.getPokemon(pokemonId).then((pokemon) => {
+        const pokemonDetail = document.getElementById('pokemon-detail');
+        const newHtml =
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => '<li class="type ${type}">${type}</li>').join('')}
+                </ol>
+                <h3>${pokemon.name}</h3>
+                <img src="${pokemon.photo}" alt="${pokemon.name}"></img>
+            </div>
+        ;
+        pokemonDetail.innerHTML=newHtml;
+    })
+}
+
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map(convertPokemonToLi).join('')
         pokemonList.innerHTML += newHtml
-    })
+
+        const pokemonItems = document.querySelectorAll('.pokemon');
+        pokemonItems.forEach((iten) => {
+            const pokemonId = item.getAttribute('data-pokemon-id');
+            item.addEventListener('click', () => {
+                loadPokemonDetails(pokemonId);
+            });
+        });
+    });
 }
 
 loadPokemonItens(offset, limit)
